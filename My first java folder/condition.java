f6c9529a39ffe36a5d50a9fdec1b5059f867c626
abusaeidbabu <abusaeidpramanik2@gmail.com>
2026-01-03 12:38:23 +0600

    Dsa code

---
+++ My first java folder/condition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class condition {
+    public static void main(String args [])
+     {
+        System.out.println("Enter the number:");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if (age > 18) {
+            System.out.println("Adult");
+        }
+        else {
+            System.out.println("Not Adult");
+        }
+    }
+}
---
+++ My first java folder/sssssw.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sssssw {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+        switch (button) {
+            case 1:
+                System.out.println("Assalamualikum ");
+                break;
+            case 2:    
+                   System.out.println("Hello");
+                break;
+            case 3:
+                System.out.println("Namaste");
+                break;    
+            default:
+                System.out.println("Hakuna Madata");
+                break;
+
+        }
+    }
+}   
