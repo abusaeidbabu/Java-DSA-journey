3a932a12dadaf67244aad9a60b706355d74fa9e9
abusaeidbabu <abusaeidpramanik2@gmail.com>
2026-01-03 18:33:49 +0600

    Create calculator with java

---
+++ My first java folder/calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args)
+     {
+        System.out.println("Enter the value a,b:");
+        Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+   System.out.println("Oparetor:\n1.*\n2.-\n3.+\n4.%\nSelect Oparetor--");
+   int n = sc.nextInt();
+   switch (n) {
+    case 1:
+        System.out.println("The value is="+(a*b));
+        break;
+    case 2:
+        System.out.println("The value is="+(a-b));
+        break;
+    case 3:
+        System.out.println("The value is="+(a+b));
+        break;
+    case 4:
+        System.out.println("The value is="+(a%b));
+        break;            
+   
+    default:
+        System.out.println("F Y");
+        break;
+   }
+
+
+  
+
+    
+   }
+}
+
