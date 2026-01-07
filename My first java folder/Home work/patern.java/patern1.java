c215e35b0bdc34951341e584c5bb302cb858b109
abusaeidbabu <abusaeidpramanik2@gmail.com>
2026-01-08 01:17:50 +0600

    Work

---
+++ My first java folder/Home work/patern.java/patern1.java
@@ -0,0 +1,14 @@
+
+
+public class patern1 {
+public static void main(String[] args) {
+    int n=5;
+    int m=4;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            System.out.print(" *");
+        }
+        System.out.println("");
+    }
+}
+}
